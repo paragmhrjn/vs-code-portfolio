4ef2fe7aa954900783a124438363560f23a77cda
ParagMhrjn <chandra_parag@hotmail.com>
2025-07-01 17:45:24 +0200

    new setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Vs-Code_Porfolio",
+  base:"/Vs-Code-Porfolio",
   plugins: [react(), tailwindcss()],
 })
