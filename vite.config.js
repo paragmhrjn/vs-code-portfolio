c6813aa6a8e0ed557b6e076bbfc8216ddbea79ac
ParagMhrjn <chandra_parag@hotmail.com>
2025-07-01 18:29:32 +0200

    updates config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/vs-code-porfolio",
+  base:"/vs-code-portfolio/",
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',      // gets deployed to github pages
